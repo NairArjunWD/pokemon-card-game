6576c1455af9e03a17e9564d07f00913a2fe5407
Arjun Nair <nair.arjun47@yahoo.com>
2019-12-18 22:34:05 -0800

    Editted page appears

--- pokemon-card-game/src/App.js
+++ pokemon-card-game/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Pokecard from './Pokecard';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Pokecard />
       </div>
     );
   }
---
+++ pokemon-card-game/src/Pokecard.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Pokecard extends Component {
+    render() {
+        return (
+            <h1>Pokemon, The Unofficial Trading Card Game</h1>
+        )
+    }
+}
+
+export default Pokecard;
