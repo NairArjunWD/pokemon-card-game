d2b416268a37e9d5d8db1a5eb1cc79c08dfece5e
Arjun Nair <nair.arjun47@yahoo.com>
2020-01-02 22:43:23 -0800

    Add more pokemon and chang styling

--- pokemon-card-game/src/Pokegame.js
+++ pokemon-card-game/src/Pokegame.js
@@ -4,14 +4,18 @@ import Pokedex from './Pokedex';
 class Pokegame extends Component {
     static defaultProps = {
         pokemon: [
-            { id: 4, name: 'Charmander', type: 'fire', base_experience: 62 },
-            { id: 7, name: 'Squirtle', type: 'water', base_experience: 63 },
-            { id: 11, name: 'Metapod', type: 'bug', base_experience: 72 },
-            { id: 12, name: 'Butterfree', type: 'flying', base_experience: 178 },
-            { id: 25, name: 'Pikachu', type: 'electric', base_experience: 112 },
-            { id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95 },
-            { id: 94, name: 'Gengar', type: 'poison', base_experience: 225 },
-            { id: 133, name: 'Eevee', type: 'normal', base_experience: 65 }
+            { id: 4, name: 'Charmander', type: 'fire', hp: 10, base_experience: 62 },
+            { id: 5, name: 'Charmeleon', type: 'fire', hp: 10, base_experience: 62 },
+            { id: 6, name: 'Charizard', type: 'fire', hp: 10, base_experience: 62 },
+            { id: 7, name: 'Squirtle', type: 'water', hp: 10, base_experience: 63 },
+            { id: 8, name: 'Wartortle', type: 'water', hp: 10, base_experience: 63 },
+            { id: 9, name: 'Blastoise', type: 'water', hp: 10, base_experience: 63 },
+            { id: 11, name: 'Metapod', type: 'bug', hp: 10, base_experience: 72 },
+            { id: 12, name: 'Butterfree', type: 'flying', hp: 10, base_experience: 178 },
+            { id: 25, name: 'Pikachu', type: 'electric', hp: 10, base_experience: 112 },
+            { id: 39, name: 'Jigglypuff', type: 'normal', hp: 10,base_experience: 95 },
+            { id: 94, name: 'Gengar', type: 'poison', hp: 10, base_experience: 225 },
+            { id: 133, name: 'Eevee', type: 'normal', hp: 10, base_experience: 65 }
         ]
     };
     render() {
